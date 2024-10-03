fe3ec3f1ffa95df874729f978e54d070cef06725
petscheit <etscheitp@gmail.com>
2024-10-03 10:18:09 +0200

    feat: experiment withdynamic layout

--- Makefile
+++ Makefile
@@ -7,6 +7,9 @@ buildx:
 run:
 	./scripts/cairo_run.sh
 
+pie-run:
+	./scripts/cairo_run.sh --pie
+
 build_and_run:
 	./scripts/cairo_compile.sh
 	./scripts/cairo_run.sh
--- scripts/cairo_run.sh
+++ scripts/cairo_run.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
-# Check if input file path is provided, otherwise use default
-if [ -z "$1" ]; then
-    INPUT_FILE="input.json"
-else
-    INPUT_FILE="$1"
-fi
+INPUT_FILE="input.json"
+PIE_FLAG=""
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --pie)
+            PIE_FLAG="--cairo_pie_output=pie.zip"
+            shift
+            ;;
+        *)
+            INPUT_FILE="$1"
+            shift
+            ;;
+    esac
+done
 
 echo "Running Cairo program..."
 
 # Start timing
 start_time=$(date +%s.%N)
 
-cairo-run --program=cairo/build/main.json --program_input="$INPUT_FILE" --layout=all_cairo --print_output --print_info
+cairo-run --program=cairo/build/main.json \
+    --program_input="$INPUT_FILE" \
+    --layout=all_cairo \
+    --print_info \
+    $PIE_FLAG
+    # --cairo_layout_params_file=dynamic_params.json \
+    # --proof_mode \
 
 # End timing
 end_time=$(date +%s.%N)
