92ae82e2267f5a6569a58aeda290d4e25c2d58f8
petscheit <etscheitp@gmail.com>
2024-11-27 13:46:52 +0100

    chore: added new committee proof skeleton

--- scripts/cairo_compile.sh
+++ scripts/cairo_compile.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo "Compiling Cairo Program"
-cairo-compile --cairo_path=cairo/packages/garaga_zero/src "cairo/src/main.cairo" --output "cairo/build/main.json" --proof_mode
+PROGRAM_PATH=${1:-"cairo/src/main.cairo"}  # Default to main.cairo if no argument provided
+OUTPUT_NAME=$(basename "$PROGRAM_PATH" .cairo)  # Extract filename without path and extension
 
+echo "Compiling Cairo Program: $PROGRAM_PATH"
+cairo-compile --cairo_path=cairo/packages/garaga_zero/src "$PROGRAM_PATH" --output "cairo/build/${OUTPUT_NAME}.json" --proof_mode
 
 if [ $? -eq 0 ]; then
     echo "Compilation Successful!"
-    
 fi
