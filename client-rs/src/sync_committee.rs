ef679dc7941bd5bf6531e3e5be9c966552bc3a00
petscheit <etscheitp@gmail.com>
2024-11-27 12:45:53 +0100

    chore: cleanup

--- client-rs/src/sync_committee.rs
+++ client-rs/src/sync_committee.rs
@@ -1,7 +1,6 @@
 use crate::types::SyncCommitteeUpdateProof;
 use beacon_state_proof::error::Error as BeaconStateProofError;
 use beacon_state_proof::state_proof_fetcher::StateProofFetcher;
-use beacon_state_proof::state_proof_fetcher::TreeHash;
 pub struct SyncCommitteeUpdate {}
 
 impl SyncCommitteeUpdate {
@@ -9,10 +8,6 @@ impl SyncCommitteeUpdate {
         rpc_url: &String,
         slot: u64,
     ) -> Result<SyncCommitteeUpdateProof, SyncCommitteeUpdateError> {
-        // // We only support checkpoint sync committee updates
-        // if slot % 32 != 0 {
-        //     return Err(SyncCommitteeUpdateError::NoCheckpointSlotRequested);
-        // }
 
         let state_proof_fetcher = StateProofFetcher::new(rpc_url.clone());
         let proof = state_proof_fetcher
--- client-rs/src/types.rs
+++ client-rs/src/types.rs
@@ -1,9 +1,9 @@
-use crate::sync_committee::SyncCommitteeUpdateError;
-use crate::utils::merkle;
 use alloy_primitives::{FixedBytes, U64};
 use beacon_state_proof::state_proof_fetcher::{SyncCommitteeProof, TreeHash};
+use sha2::{Sha256, Digest};
+use crate::sync_committee::SyncCommitteeUpdateError;
 use serde::Serialize;
-use sha2::{Digest, Sha256};
+use crate::utils::merkle;
 
 #[derive(Debug)]
 pub struct SyncCommitteeUpdateProof {
@@ -19,23 +19,22 @@ pub struct SyncCommitteeUpdateProof {
 
 impl SyncCommitteeUpdateProof {
     pub fn compute_state_root(&self) -> Result<FixedBytes<32>, SyncCommitteeUpdateError> {
+
         // Since the keys are 48 bytes, we pad them to 64 bytes and hash them for the root
         let mut padded_aggregate = vec![0u8; 64];
         padded_aggregate[..48].copy_from_slice(&self.next_aggregate_sync_committee[..]);
-        let aggregate_root: FixedBytes<32> =
-            FixedBytes::from_slice(&Sha256::digest(&padded_aggregate));
+        let aggregate_root: FixedBytes<32> = FixedBytes::from_slice(&Sha256::digest(&padded_aggregate));
 
         let mut leaf_data = [0u8; 64];
         leaf_data[0..32].copy_from_slice(self.committee_keys_root.as_slice());
         leaf_data[32..64].copy_from_slice(aggregate_root.as_slice());
         let leaf = FixedBytes::from_slice(&Sha256::digest(&leaf_data));
-
-        let state_root =
-            merkle::hash_merkle_path(self.next_sync_committee_branch.clone(), leaf, 55);
-
+        
+        let state_root = merkle::hash_merkle_path(self.next_sync_committee_branch.clone(), leaf, 55);
         Ok(state_root)
-        // Ok(aggregate_root)
     }
+
+
 }
 
 impl From<SyncCommitteeProof> for SyncCommitteeUpdateProof {
@@ -43,21 +42,15 @@ impl From<SyncCommitteeProof> for SyncCommitteeUpdateProof {
         let committee_keys_root = &committee_proof.next_sync_committee.pubkeys.tree_hash_root();
         Self {
             beacon_slot: U64::from(committee_proof.slot),
-            next_sync_committee_branch: committee_proof
-                .proof
-                .into_iter()
+            next_sync_committee_branch: committee_proof.proof.into_iter()
                 .map(|bytes| FixedBytes::from_slice(&bytes.as_bytes()))
                 .collect(),
-            next_aggregate_sync_committee: FixedBytes::from_slice(
-                &committee_proof
-                    .next_sync_committee
-                    .aggregate_pubkey
-                    .as_serialized(),
-            ),
+            next_aggregate_sync_committee: FixedBytes::from_slice(&committee_proof.next_sync_committee.aggregate_pubkey.as_serialized()),
             committee_keys_root: FixedBytes::from_slice(committee_keys_root.as_bytes()),
         }
     }
 }
+
 
 impl Serialize for SyncCommitteeUpdateProof {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
@@ -65,23 +58,14 @@ impl Serialize for SyncCommitteeUpdateProof {
         S: serde::Serializer,
     {
         use serde::ser::SerializeStruct;
-
+        
         let mut state = serializer.serialize_struct("SyncCommitteeUpdateProof", 4)?;
-
+        
         state.serialize_field("beacon_slot", &self.beacon_slot)?;
-        state.serialize_field(
-            "next_sync_committee_branch",
-            &self.next_sync_committee_branch,
-        )?;
-        state.serialize_field(
-            "next_aggregate_sync_committee",
-            &hex::encode(self.next_aggregate_sync_committee),
-        )?;
-        state.serialize_field(
-            "committee_keys_root",
-            &hex::encode(self.committee_keys_root),
-        )?;
-
+        state.serialize_field("next_sync_committee_branch", &self.next_sync_committee_branch)?;
+        state.serialize_field("next_aggregate_sync_committee", &hex::encode(self.next_aggregate_sync_committee))?;
+        state.serialize_field("committee_keys_root", &hex::encode(self.committee_keys_root))?;
+        
         state.end()
     }
 }
